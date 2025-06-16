ee81bd4f8470fc95fb48e28f67375c89127ecfe6
Diego Vallejos <diego.v@hotmail.com>
2025-06-16 01:41:54 -0300

    added: se agrega componentes de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Guitar from "./components/Guitar.jsx";
+import { db } from "./data/db";
+
 import './App.css'
 
 function App() {
+
+    const [data] = useState(db);
+
     return (
         <>
-            <h1>Bienvenidos</h1>
+            <Header />
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+                <div className="row mt-5">
+                    { data.map( guitar => (
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
+                        />
+                    ))}
+                </div>
+            </main>
+            <Footer />
         </>
     )
 }
